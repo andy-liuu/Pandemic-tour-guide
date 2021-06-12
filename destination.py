c99d9b2595dc292d14b760bb138278757ca148e3
andy-liuu <59021924+andy-liuu@users.noreply.github.com>
2021-06-12 15:38:15 -0400

    changes to return
    changed return statement so info can be used in other functions, changes to keys (consistency with other funcs), added tester code

--- destination.py
+++ destination.py
@@ -1,16 +1,25 @@
 def destinations(key):
-    country = {'Canada':["CN Tower","Parliament Hill","Banff National Park","Niagara Falls"], 
-           'India':["Mumbai", "Jaipur", "Taj Mahal", "Ranthambore National Park"],
-           'China':["Great Wall", "Forbidden City", "Tiananmen Square", "Temple of Heaven"],
+
+    key = key.strip().upper()
+    
+    country = {'CANADA':["CN Tower","Parliament Hill","Banff National Park","Niagara Falls"], 
+           'INDIA':["Mumbai", "Jaipur", "Taj Mahal", "Ranthambore National Park"],
+           'CHINA':["Great Wall", "Forbidden City", "Tiananmen Square", "Temple of Heaven"],
            'USA':["Grand Canyon", "Yosemite National Park", "Lake Tahoe", "Golden Gate Bridge"],
-           'Russia':["State Hermitage Museum", "St. Basil's Cathedral", "The Moscow Kremlin", "Red Square"],
-           'Spain':["La Sagrada Familia", "Park Guell", "Alhambra", "Museo Nacional de Prado"],
-           'Thailand':["The Grand Palace", "Wat Arun Ratchawararam Ratchaworamahawihan","Railay Beach","Chatuchak Weekend Market"],
-           'Australia':["Great Barrier Reef", "Sydney Opera House", "Uluru", "Kakadu National Park"],
-           'Egypt':["The Egyptian Museum", "Valley of the Kings", "Karmak", "Great Sphinx of Giza"],
-           'South Africa':["Kruger National Park", "Maclear's Beacon", "KirstenBosch National Botanical Garden", "Boulder's Beach"],
-           'Mexico':["Chichen Itza", "Tulum Archaeological Zone", "Frida Kahlo Museum", "Xcaret Park"],
-           'Brazil':["Christ the Redeemer", "Sugarloaf Mountain","Museum of Art of Sao Paulo Assis Chateaubriand", "Copacabana Beach"],
-           'Saudi Arabia':["Masjid al-Haram", "Abraj Al-Bait Towers", "Center Point", "Masmak Fortress"],
-           'Ethiopia':["Simien Mountains National Park", "Rock-Hewn Churches, Lalibela", "Blue Nile Falls", "Bale Mountains National Park"]}
-    print("Tourist Attractions in", key, ":", country[key])
+           'RUSSIA':["State Hermitage Museum", "St. Basil's Cathedral", "The Moscow Kremlin", "Red Square"],
+           'SPAIN':["La Sagrada Familia", "Park Guell", "Alhambra", "Museo Nacional de Prado"],
+           'THAILAND':["The Grand Palace", "Wat Arun Ratchawararam Ratchaworamahawihan","Railay Beach","Chatuchak Weekend Market"],
+           'AUSTRALIA':["Great Barrier Reef", "Sydney Opera House", "Uluru", "Kakadu National Park"],
+           'EGYPT':["The Egyptian Museum", "Valley of the Kings", "Karmak", "Great Sphinx of Giza"],
+           'SOUTH AFRICA':["Kruger National Park", "Maclear's Beacon", "KirstenBosch National Botanical Garden", "Boulder's Beach"],
+           'MEXICO':["Chichen Itza", "Tulum Archaeological Zone", "Frida Kahlo Museum", "Xcaret Park"],
+           'BRAZIL':["Christ the Redeemer", "Sugarloaf Mountain","Museum of Art of Sao Paulo Assis Chateaubriand", "Copacabana Beach"],
+           'SAUDI ARABIA':["Masjid al-Haram", "Abraj Al-Bait Towers", "Center Point", "Masmak Fortress"],
+           'ETHIOPIA':["Simien Mountains National Park", "Rock-Hewn Churches, Lalibela", "Blue Nile Falls", "Bale Mountains National Park"]}
+    return country[key]
+        
+
+#tester code
+if __name__ == "__main__":
+    print(destinations("canada"))
+    print(destinations("mexICO"))
